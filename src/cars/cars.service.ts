2a55ce9d5dede845705bdf7197d3506d0deb3eac
goncalveshigino <goncalveshigino20@gmail.com>
2023-05-21 19:05:24 +0100

    perf: adicionei  decorators, injecao de service, pipes e exceptions filter

---
+++ src/cars/cars.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+
+    private cars = [
+
+        {
+            id: 1, 
+            brand: 'Toyota', 
+            model: 'Corola'
+        }, 
+
+        {
+            id: 2, 
+            brand: 'Honda', 
+            model: 'Civic'
+        }, 
+
+        {
+            id: 3, 
+            brand: 'Jeep', 
+            model: 'Cherokee'
+        }
+
+    ];
+
+
+    findAll(){
+        return this.cars
+    }
+
+    findOneById( id: number ) {
+        const car = this.cars.find( car => car.id === id );
+
+        if( !car ) throw new NotFoundException(`Car with id ''${ id } not found`);
+        
+        return car;
+        //return this.cars[id]
+    }
+}
