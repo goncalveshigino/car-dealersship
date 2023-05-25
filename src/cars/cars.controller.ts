c6f4ef010c0e8d15654e1bd9d1ae2eaedb95eeaa
goncalveshigino <goncalveshigino20@gmail.com>
2023-05-25 11:46:03 +0100

    chore: colocando Validation Pipe no nivel mais alto do app

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller,Delete,Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller,Delete,Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCartDto } from './dto/create-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -13,14 +14,13 @@ export class CarsController {
     }
     
     @Get(':id')
-    getCarById( @Param('id') id: string  ) { 
+    getCarById( @Param('id', ParseUUIDPipe ) id: string  ) { 
         return this.carsService.findOneById( id )
     }
 
     @Post()
-    createCar( @Body() body: any ){
-
-        return body
+    createCar( @Body() createCartDto: CreateCartDto ){
+      return createCartDto
     }
 
     @Patch(':id')
