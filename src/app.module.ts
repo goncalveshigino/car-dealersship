09a72632e8a26ab5aed3ee1cc4cbbc74e6af42e0
goncalveshigino <goncalveshigino20@gmail.com>
2023-05-28 18:38:48 +0100

    chore: crud completo de Brands

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CarsModule } from './cars/cars.module';
+import { BrandsModule } from './brands/brands.module';
 
 
 @Module({
-  imports: [CarsModule],
+  imports: [CarsModule, BrandsModule ],
   controllers: [],
   providers: [],
   exports: []
